c45064579ad052df3c1a88cb1d8e277410111027
Pallavi Soam <pallavisoam35@gmail.com>
2023-01-09 18:50:57 +0530

    port updated

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 var cors = require('cors')
 const sendSms = require('./twilio')
-
+require('dotenv').config()
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(bodyParser.json())
 
-const port = 8000
+// const port = 8000
 
 // Create users endpoint
 app.post('/api/users', (req, res) => {
@@ -25,7 +25,7 @@ app.post('/api/users', (req, res) => {
         data: user
     })
 })
-
+const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
